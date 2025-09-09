0bb2e46c9fb4343584ef60e193d52fa7bf5a3c21
karthik13vk <vijayakarthik13@gmail.com>
2025-09-09 22:44:47 +0530

    sdsd

--- src/components/Sitemarquee/Sitemarquee.jsx
+++ src/components/Sitemarquee/Sitemarquee.jsx
@@ -3,12 +3,13 @@ import Marquee from 'react-fast-marquee';
 const Sitemarquee = () => {
     return (
         <div className='py-6 bg-primary'>
-            <Marquee speed={50} gradient={false}>
-                <span className="mx-8 text-6xl text-transparent"
+            <Marquee speed={50} gradient={false}  style={{   overflowY: 'hidden',}}>
+                <span className="mx-8 overflow-y-hidden text-6xl text-transparent"
                     style={{
                         color: 'transparent',
                         WebkitTextStroke: '1px white',
                         fontSize: '50px',
+                        overflowY: 'hidden',
                         fontWeight: 'normal',
                     }}>Rituals</span>
                 <span className="mx-8 text-6xl"
@@ -16,6 +17,7 @@ const Sitemarquee = () => {
                         color: 'white',
                         // WebkitTextStroke: '1px white',
                         fontSize: '50px',
+                        overflowY: 'hidden',
                         fontWeight: 'normal',
                     }}>Bridal Portraits</span>
                 <span className="mx-8 text-6xl "
@@ -23,6 +25,7 @@ const Sitemarquee = () => {
                         color: 'transparent',
                         WebkitTextStroke: '1px white',
                         fontSize: '50px',
+                        overflowY: 'hidden',
                         fontWeight: 'normal',
                     }}>Groom Portraits</span>
                 <span className="mx-8 text-6xl"
@@ -30,6 +33,7 @@ const Sitemarquee = () => {
                         color: 'white',
                         // WebkitTextStroke: '1px white',
                         fontSize: '50px',
+                        overflowY: 'hidden',
                         fontWeight: 'normal',
                     }}>Candid Moments</span>
                 <span className="mx-8 text-6xl"
@@ -37,6 +41,7 @@ const Sitemarquee = () => {
                         color: 'transparent',
                         WebkitTextStroke: '1px white',
                         fontSize: '50px',
+                        overflowY: 'hidden',
                         fontWeight: 'normal',
                     }}>Baby shower</span>
                 <span className="mx-8 text-6xl"
@@ -44,6 +49,7 @@ const Sitemarquee = () => {
                         color: 'white',
                         // WebkitTextStroke: '1px white',
                         fontSize: '50px',
+                        overflowY: 'hidden',
                         fontWeight: 'normal',
                     }}>Wedding Reception</span>
                 <span className="mx-8 text-6xl"
@@ -51,6 +57,7 @@ const Sitemarquee = () => {
                         color: 'transparent',
                         WebkitTextStroke: '1px white',
                         fontSize: '50px',
+                        overflowY: 'hidden',
                         fontWeight: 'normal',
                     }}>Couple Portraits</span>
                 <span className="mx-8 text-6xl"
@@ -58,6 +65,7 @@ const Sitemarquee = () => {
                         color: 'white',
                         // WebkitTextStroke: '1px white',
                         fontSize: '50px',
+                        overflowY: 'hidden',
                         fontWeight: 'normal',
                     }}>Outdoor </span>
             </Marquee>
