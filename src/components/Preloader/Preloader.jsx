4cf193742403eb6f044fbfd3f9aae648a6d3c1c3
karthik13vk <vijayakarthik13@gmail.com>
2025-09-30 19:19:37 +0530

    preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './Preloader.css';
+import preloaderimg from './../../assets/img/FooterLogo.png';
 
 const Preloader = () => {
   return (
-    <div className="modern-preloader">
-      <div className="loader-content">
-        <h1 className="loader-title">ESK</h1>
-        <div className="loader-bar">
-          <div className="bar-fill"></div>
-        </div>
+    // <div className="modern-preloader">
+    //   <div className="loader-content">
+    //     <h1 className="w-50 loader-title">
+    //       <img src={preloaderimg} alt="img" />
+    //     </h1>
+    //     <div className="loader-bar">
+    //       <div className="bar-fill"></div>
+    //     </div>
+    //   </div>
+    // </div>
+     <div className="heartbeat-preloader">
+      <div className="heartbeat-wrapper">
+        <img src={preloaderimg} alt="Loading" className="heartbeat-logo" />
+        {/* <p className="heartbeat-text">Loading...</p> */}
       </div>
     </div>
   );
